2150d85feb4b74ad2b3a985b839a7def1aa19654
lyttam <annemlyttle@gmail.com>
2016-10-17 14:37:56 -0700

    SISRP-25362 - adds handling for CS API error responses with status=200

--- spec/models/campus_solutions/proxy_spec.rb
+++ spec/models/campus_solutions/proxy_spec.rb
@@ -9,13 +9,22 @@ describe CampusSolutions::Proxy do
       allow(response).to receive(:body).and_return double force_encoding: nil
       allow(response).to receive(:parsed_response).and_return feed
     }
-    context 'errmsgtext in response' do
-      let(:feed) { { 'ERRMSGTEXT' => 'Wicked problems' } }
+    shared_examples 'a proxy that handles errors' do
       it 'should flag error condition' do
         processed_feed = HashConverter.downcase_and_camelize feed
         expect(subject.is_errored? processed_feed).to be true
         expect(subject.get).to eq ({ statusCode: 400, errored: true, feed: processed_feed })
       end
+    end
+
+    context 'errmsgtext in response' do
+      let(:feed) { { 'ERRMSGTEXT' => 'Wicked problems' } }
+      it_behaves_like 'a proxy that handles errors'
+    end
+
+    context 'invalid response' do
+      let(:feed) { '<HTML><HEAD><TITLE>RESTListeningConnector</TITLE></HEAD><BODY>Unable to find a Routing corresponding to the incoming request message.</BODY></HTML>' }
+      it_behaves_like 'a proxy that handles errors'
     end
   end
 end
