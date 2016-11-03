275c9aff45e212152a7a2aa08d88b41aa96d6d63
John Crossman <johncrossman@berkeley.edu>
2016-11-03 10:42:20 -0700

    CLC-6475, JVM - during sync-canvas-new-users script - gets large Max Heap Size

--- script/sync-canvas-new-users.sh
+++ script/sync-canvas-new-users.sh
@@ -3,26 +3,26 @@
 
 # Make sure the normal shell environment is in place, since it may not be
 # when running as a cron job.
-source "$HOME/.bash_profile"
+source "${HOME}/.bash_profile"
 
 cd $( dirname "${BASH_SOURCE[0]}" )/..
 
-LOG=`date +"$PWD/log/canvas_new_user_sync_%Y-%m-%d.log"`
+LOG=$(date +"$PWD/log/canvas_new_user_sync_%Y-%m-%d.log")
 LOGIT="tee -a $LOG"
 
 # Enable rvm and use the correct Ruby version and gem set.
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+[[ -s "${HOME}/.rvm/scripts/rvm" ]] && . "${HOME}/.rvm/scripts/rvm"
 source .rvmrc
 
 export RAILS_ENV=${RAILS_ENV:-production}
 export LOGGER_STDOUT=only
 export LOGGER_LEVEL=INFO
-export JRUBY_OPTS="--dev"
+export JRUBY_OPTS="--dev -J-Xmn512m -J-Xms2048m -J-Xmx2048m -J-XX:+HeapDumpOnOutOfMemoryError"
 
-echo | $LOGIT
-echo "------------------------------------------" | $LOGIT
-echo "`date`: About to run the new campus user sync script..." | $LOGIT
+echo | ${LOGIT}
+echo "------------------------------------------" | ${LOGIT}
+echo "$(date): About to run the new campus user sync script..." | ${LOGIT}
 
 cd deploy
 
-bundle exec rake canvas:new_user_sync | $LOGIT
+bundle exec rake canvas:new_user_sync | ${LOGIT}
