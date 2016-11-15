da51cce7b70a0ac1b630663fa99a0f375ec96702
Nader Kutub <naderk@berkeley.edu>
2016-11-15 11:52:50 -0800

    SISRP-27680 - My Academics (instr) - Semesters [Teaching] (Final Grading Updates)

--- spec/models/campus_solutions/grading_spec.rb
+++ spec/models/campus_solutions/grading_spec.rb
@@ -1,6 +1,7 @@
 describe CampusSolutions::Grading do
 
-  let(:user_id) { '123456' }
+  let(:user_id) { random_id }
+  let(:user_cs_id) { random_id }
 
   shared_examples 'a proxy that gets data' do
     subject { proxy.get }
@@ -13,6 +14,10 @@ describe CampusSolutions::Grading do
   end
 
   context 'mock proxy' do
+    before do
+      allow(CalnetCrosswalk::ByUid).to receive(:new).with(user_id: user_id).and_return(
+        double(lookup_campus_solutions_id: user_cs_id))
+    end
     let(:proxy) { CampusSolutions::Grading.new(user_id: user_id, fake: true) }
     it_should_behave_like 'a proxy that gets data'
   end
