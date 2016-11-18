ee54ed7099bdb46e5ab6f6079158e502dd003652
David Cheng <davidlimcheng@gmail.com>
2016-11-18 13:23:13 -0800

    SISRP-27748 - 7.3 Transfer Credit - Link to Student Overview (advisor)

--- app/models/my_academics/transfer_credit.rb
+++ app/models/my_academics/transfer_credit.rb
@@ -14,7 +14,19 @@ module MyAcademics
         nonadjusted_units = credit[:unitsNonAdjusted].to_f
         response[:ucTransferCrseSch][:unitsTotal] = adjusted_units + nonadjusted_units
       end
+      response[:tcReportLink] = fetch_tc_report_link
       response
+    end
+
+    def fetch_tc_report_link
+      lookup_student_id
+      fetch_link('UC_CX_XFER_CREDIT_REPORT_STDNT', { :EMPLID => @campus_solutions_id })
+    end
+
+    def lookup_student_id
+      if @uid
+        @campus_solutions_id = CalnetCrosswalk::ByUid.new(user_id: @uid).lookup_campus_solutions_id
+      end
     end
 
   end
