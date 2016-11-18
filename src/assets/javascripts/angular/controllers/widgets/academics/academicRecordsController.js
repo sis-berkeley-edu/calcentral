fc31a71f3103b516d9155c6ef0b41c611a09bec7
David Cheng <davidlimcheng@gmail.com>
2016-11-18 10:22:27 -0800

    SISRP-26741 - 7.3: Academic Records on My Academics (Student)

--- src/assets/javascripts/angular/controllers/widgets/academics/academicRecordsController.js
+++ src/assets/javascripts/angular/controllers/widgets/academics/academicRecordsController.js
@@ -3,7 +3,7 @@
 var angular = require('angular');
 var _ = require('lodash');
 
-angular.module('calcentral.controllers').controller('AcademicRecordsController', function(academicRecordsFactory, apiService, $scope) {
+angular.module('calcentral.controllers').controller('AcademicRecordsController', function(academicRecordsFactory, apiService, csLinkFactory, $scope) {
 
   $scope.officialTranscript = {
     postParams: {},
@@ -11,6 +11,11 @@ angular.module('calcentral.controllers').controller('AcademicRecordsController',
     postUrlHover: 'Request Transcript',
     isLoading: true
   };
+  $scope.lawTranscriptLink = {
+    link: 'http://www.law.berkeley.edu/php-programs/registrar/forms/transcriptrequestform.php',
+    title: 'Request Law Transcript'
+  };
+  $scope.lawUnofficialTranscriptLink = {};
 
   /**
    * Constructs a post request to Credentials Solutions, as outlined by the Credentials Solutions documentation seen in
@@ -52,11 +57,21 @@ angular.module('calcentral.controllers').controller('AcademicRecordsController',
     });
   };
 
+  var fetchLawUnofficialTranscriptLink = function() {
+    csLinkFactory.getLink({
+      urlId: 'UC_CX_RQST_UNOFF_LAW_TRANSCRPT'
+    })
+    .then(function(data) {
+      $scope.lawUnofficialTranscriptLink = _.get(data, 'data.link');
+    });
+  };
+
   var loadTranscriptData = function() {
     academicRecordsFactory.getTranscriptData()
       .then(function(data) {
         parseData(data);
       })
+      .then(fetchLawUnofficialTranscriptLink)
       .finally(function() {
         $scope.officialTranscript.isLoading = false;
       });
