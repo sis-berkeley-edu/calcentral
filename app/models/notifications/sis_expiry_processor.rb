a62a8f3ece0b1b93b9fe7993472f58872943f647
David Cheng <davidlimcheng@gmail.com>
2016-12-08 16:25:32 -0800

    SISRP-28699 - 7.4: ENF for Grade Roster Status and Academic Plan Card

--- app/models/notifications/sis_expiry_processor.rb
+++ app/models/notifications/sis_expiry_processor.rb
@@ -5,8 +5,12 @@ module Notifications
     def process(event, timestamp)
       return false unless accept? event
       logger.debug "Processing event: #{event}; timestamp = #{timestamp}"
-      if (expiry_module = get_expiry event) && (uid = get_uid event)
+      if (expiry_module = get_expiry event)
+        if (uid = (event['topic'] == 'sis:faculty:grade-roster') ? (get_instructor_uids event) : (get_uid event))
         expiry_module.expire uid
+        end
+      else
+        logger.warn "Event topic #{event['topic']} not recognized"
       end
     end
 
@@ -30,6 +34,22 @@ module Notifications
       uid
     end
 
+    def get_instructor_uids(event)
+      uids = []
+      if (section_data = event['payload'] && event['payload']['student'] && event['payload']['student']['StudentId'].to_s.split('|'))
+        if (term_id = section_data[0]) && (section_id = section_data[1])
+          instructors = EdoOracle::Queries.get_section_instructors(term_id, section_id)
+          instructors.each do |instructor|
+            uids.push instructor['ldap_uid'].to_i
+          end
+          logger.error "No Instructor UIDs found for section #{section_id}, term #{term_id}"
+        end
+      else
+        logger.error "Could not parse Instructor UIDs from event #{event}"
+      end
+      uids
+    end
+
     #TODO Mapping of event topics to expiry modules is incomplete.
     EXPIRY_BY_TOPIC = {
       'sis:staff:advisor' => CampusSolutions::AdvisingExpiry,
@@ -43,7 +63,8 @@ module Notifications
       'sis:student:finaid' => CampusSolutions::FinancialAidExpiry,
       'sis:student:financials' => CampusSolutions::MyBilling,
       'sis:student:messages' => MyActivities::Merged,
-      'sis:student:serviceindicator' => HubEdos::AcademicStatus
+      'sis:student:serviceindicator' => HubEdos::AcademicStatus,
+      'sis:faculty:grade-roster' => CampusSolutions::SectionGradesExpiry
     }
   end
 end
