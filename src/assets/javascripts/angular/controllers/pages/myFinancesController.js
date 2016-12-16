65c5321a85d73d1e9283df67f40d0821eab2d493
lyttam <annemlyttle@gmail.com>
2016-12-15 17:55:02 -0800

    SISRP-27699 - My Finances: Remove Financial Resources and Scholarships Summary

--- src/assets/javascripts/angular/controllers/pages/myFinancesController.js
+++ src/assets/javascripts/angular/controllers/pages/myFinancesController.js
@@ -1,10 +1,25 @@
 'use strict';
 
+var _ = require('lodash');
 var angular = require('angular');
 
 /**
  * My Finances controller
  */
-angular.module('calcentral.controllers').controller('MyFinancesController', function(apiService) {
+angular.module('calcentral.controllers').controller('MyFinancesController', function($scope, apiService, academicStatusFactory) {
   apiService.util.setTitle('My Finances');
+
+  $scope.academicStatus = {
+    roles: {}
+  };
+
+  var parseAcademicStatusRoles = function(data) {
+    _.extend($scope.academicStatus.roles, _.get(data, 'data.feed.student.roles'));
+  };
+
+  $scope.$on('calcentral.api.user.isAuthenticated', function(event, isAuthenticated) {
+    if (isAuthenticated && apiService.user.profile.hasFinancialsTab) {
+      academicStatusFactory.getAcademicStatus($scope).then(parseAcademicStatusRoles);
+    }
+  });
 });
