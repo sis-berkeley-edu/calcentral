d5cbeab90db88852b98ec2f261a7e5c88d1dea0c
David Cheng <davidlimcheng@gmail.com>
2017-01-23 11:42:28 -0800

    SISRP-27828 - undefined method [] for nil:NilClass: FilteredForAdvisor > my_academics/transfer_credit.rb:12

--- app/models/my_academics/transfer_credit.rb
+++ app/models/my_academics/transfer_credit.rb
@@ -1,6 +1,8 @@
 module MyAcademics
   class TransferCredit
     include AcademicsModule
+    include ClassLogger
+    include User::Student
 
     def merge(data)
       data[:transferCredit] = transfer_credit
@@ -9,24 +11,18 @@ module MyAcademics
     def transfer_credit
       response = CampusSolutions::TransferCredit.new(user_id: @uid).get
       response = response.try(:[], :feed).try(:[], :root).try(:[], :ucTransferCredits).try(:[], :transferCredit)
-      if (credit = response[:ucTransferCrseSch])
+      if (credit = response.try(:[], :ucTransferCrseSch))
         adjusted_units = credit[:unitsAdjust].to_f
         nonadjusted_units = credit[:unitsNonAdjusted].to_f
         response[:ucTransferCrseSch][:unitsTotal] = adjusted_units + nonadjusted_units
       end
-      response[:tcReportLink] = fetch_tc_report_link
+      response.update({tcReportLink: fetch_tc_report_link}) if response
       response
     end
 
     def fetch_tc_report_link
-      lookup_student_id
-      fetch_link('UC_CX_XFER_CREDIT_REPORT_STDNT', { :EMPLID => @campus_solutions_id })
-    end
-
-    def lookup_student_id
-      if @uid
-        @campus_solutions_id = CalnetCrosswalk::ByUid.new(user_id: @uid).lookup_campus_solutions_id
-      end
+      campus_solutions_id = lookup_student_id
+      fetch_link('UC_CX_XFER_CREDIT_REPORT_STDNT', { :EMPLID => campus_solutions_id })
     end
 
   end
