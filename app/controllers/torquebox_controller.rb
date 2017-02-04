01ef2d8f53785201e7b068abaea3611459193090
Ray Davis <ray@media.berkeley.edu>
2017-02-04 06:47:25 -0800

    SISRP-29888 Gemfile fix for CI tests; style fixes

--- app/controllers/torquebox_controller.rb
+++ app/controllers/torquebox_controller.rb
@@ -5,22 +5,22 @@ class TorqueboxController < ApplicationController
   before_filter :check_permission
 
   def stats
-    result = TorqueboxInspector.new().torquebox_status
+    result = TorqueboxInspector.new.torquebox_status
     render json: {status: result}.to_json
   end
 
   def bg
-    result = TorqueboxInspector.new().bg_status
+    result = TorqueboxInspector.new.bg_status
     render json: {status: result}.to_json
   end
 
   def bg_msgs
-    result = TorqueboxInspector.new().bg_messages
+    result = TorqueboxInspector.new.bg_messages
     render json: result
   end
 
   def bg_purge
-    result = TorqueboxInspector.new().bg_purge
+    result = TorqueboxInspector.new.bg_purge
     render json: {status: result}.to_json
   end
 
@@ -34,18 +34,16 @@ class TorqueboxController < ApplicationController
   end
 
   def test_no_wait
-    worker = TorqueboxTester.new()
     times = params['times'] || 5
     pauses = params['pauses'] || 5
-    result = worker.start_background_and_return(times, pauses)
+    result = TorqueboxTester.new.start_background_and_return(times, pauses)
     render json: {result: result.to_s}.to_json
   end
 
   def test_wait
-    worker = TorqueboxTester.new()
     times = params['times'] || 5
     pauses = params['pauses'] || 5
-    result = worker.start_background_and_wait(times, pauses)
+    result = TorqueboxTester.new.start_background_and_wait(times, pauses)
     render json: {result: result.to_s}.to_json
   end
 
