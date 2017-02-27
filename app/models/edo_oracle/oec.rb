c99dfebbe7a389ff0326ac01235b8c81378731c9
Ray Davis <ray@media.berkeley.edu>
2017-02-27 15:22:19 -0800

    CLC-6594 Handle multiple meeting-schedule ranges in OEC

--- app/models/edo_oracle/oec.rb
+++ app/models/edo_oracle/oec.rb
@@ -4,79 +4,88 @@ module EdoOracle
 
     def self.get_courses(term_id, filter_clause)
       safe_query <<-SQL
-      SELECT DISTINCT
-        sec."id" AS section_id,
-        sec."primary" AS primary,
-        sec."component-code" AS instruction_format,
-        sec."sectionNumber" AS section_num,
-        sec."displayName" AS course_display_name,
-        instr."campus-uid" AS ldap_uid,
-        instr."instructor-id" AS sis_id,
-        TRIM(instr."givenName") AS first_name,
-        TRIM(instr."familyName") AS last_name,
-        instr."role-code" AS role_code,
-        instr."emailAddress" AS email_address,
-        mtg."startDate" AS start_date,
-        mtg."endDate" AS end_date,
-        TRIM(crs."transcriptTitle") AS course_title_short,
-        sec."enrolledCount" AS enrollment_count,
-        (
-          SELECT listagg("AFFILIATION_TYPE_CODE", ',') WITHIN GROUP (ORDER BY "AFFILIATION_TYPE_CODE")
-          FROM SISEDO.PERSON_AFFILIATIONV00_VW aff
+      SELECT section_id, primary, instruction_format, section_num, course_display_name,
+        ldap_uid, sis_id, first_name, last_name, role_code, email_address, course_title_short, enrollment_count,
+         affiliations, cross_listed_ccns, co_scheduled_ccns,
+         MIN(start_date) AS start_date, MAX(end_date) AS end_date from
+      (
+        SELECT DISTINCT
+          sec."id" AS section_id,
+          sec."primary" AS primary,
+          sec."component-code" AS instruction_format,
+          sec."sectionNumber" AS section_num,
+          sec."displayName" AS course_display_name,
+          instr."campus-uid" AS ldap_uid,
+          instr."instructor-id" AS sis_id,
+          TRIM(instr."givenName") AS first_name,
+          TRIM(instr."familyName") AS last_name,
+          instr."role-code" AS role_code,
+          instr."emailAddress" AS email_address,
+          mtg."startDate" AS start_date,
+          mtg."endDate" AS end_date,
+          TRIM(crs."transcriptTitle") AS course_title_short,
+          sec."enrolledCount" AS enrollment_count,
+          (
+            SELECT listagg("AFFILIATION_TYPE_CODE", ',') WITHIN GROUP (ORDER BY "AFFILIATION_TYPE_CODE")
+            FROM SISEDO.PERSON_AFFILIATIONV00_VW aff
+            WHERE
+              aff."PERSON_KEY" = instr."instructor-id" AND
+              aff."AFFILIATION_STATUS_CODE" = 'ACT' AND
+              aff."AFFILIATION_TYPE_CODE" IN ('INSTRUCTOR', 'STUDENT')
+          ) AS affiliations,
+          (
+            SELECT LISTAGG("id", ',') WITHIN GROUP (ORDER BY "id")
+            FROM SISEDO.CLASSSECTIONV00_VW sec2
+            WHERE
+              sec."cs-course-id" = sec2."cs-course-id" AND
+              sec."term-id" = sec2."term-id" AND
+              sec."session-id" = sec2."session-id" AND
+              sec."sectionNumber" = sec2."sectionNumber"
+          ) AS cross_listed_ccns,
+          CASE mtg."location-code"
+            WHEN 'INTR' THEN NULL ELSE (
+            SELECT listagg("id", ',') WITHIN GROUP (ORDER BY "id")
+            FROM SISEDO.MEETINGV00_VW mtg2 JOIN SISEDO.CLASSSECTIONV00_VW sec3 ON (
+              mtg2."location-code" = mtg."location-code" AND
+              mtg2."meetsDays" = mtg."meetsDays" AND
+              mtg2."startTime" = mtg."startTime" AND
+              mtg2."endTime" = mtg."endTime" AND
+              mtg2."startDate" = mtg."startDate" AND
+              mtg2."endDate" = mtg."endDate" AND
+              mtg2."cs-course-id" = sec3."cs-course-id" AND
+              mtg2."term-id" = sec3."term-id" AND
+              mtg2."session-id" = sec3."session-id" AND
+              mtg2."offeringNumber" = sec3."offeringNumber" AND
+              mtg2."sectionNumber" = sec3."sectionNumber")
+            )
+          END AS co_scheduled_ccns
+        FROM
+          SISEDO.CLASSSECTIONV00_VW sec
+          LEFT OUTER JOIN SISEDO.MEETINGV00_VW mtg ON (
+            mtg."cs-course-id" = sec."cs-course-id" AND
+            mtg."term-id" = sec."term-id" AND
+            mtg."session-id" = sec."session-id" AND
+            mtg."offeringNumber" = sec."offeringNumber" AND
+            mtg."sectionNumber" = sec."sectionNumber")
+          LEFT OUTER JOIN SISEDO.ASSIGNEDINSTRUCTORV00_VW instr ON (
+            instr."cs-course-id" = sec."cs-course-id" AND
+            instr."term-id" = sec."term-id" AND
+            instr."session-id" = sec."session-id" AND
+            instr."offeringNumber" = sec."offeringNumber" AND
+            instr."number" = sec."sectionNumber")
+          LEFT OUTER JOIN SISEDO.DISPLAYNAMEXLAT_MVW xlat ON (
+            xlat."classDisplayName" = sec."displayName")
+          LEFT OUTER JOIN SISEDO.API_COURSEV00_VW crs ON (
+            xlat."courseDisplayName" = crs."displayName"
+            AND crs."status-code" = 'ACTIVE')
           WHERE
-            aff."PERSON_KEY" = instr."instructor-id" AND
-            aff."AFFILIATION_STATUS_CODE" = 'ACT' AND
-            aff."AFFILIATION_TYPE_CODE" IN ('INSTRUCTOR', 'STUDENT')
-        ) AS affiliations,
-        (
-          SELECT LISTAGG("id", ',') WITHIN GROUP (ORDER BY "id")
-          FROM SISEDO.CLASSSECTIONV00_VW sec2
-          WHERE
-            sec."cs-course-id" = sec2."cs-course-id" AND
-            sec."term-id" = sec2."term-id" AND
-            sec."session-id" = sec2."session-id" AND
-            sec."sectionNumber" = sec2."sectionNumber"
-        ) AS cross_listed_ccns,
-        CASE mtg."location-code"
-          WHEN 'INTR' THEN NULL ELSE (
-          SELECT listagg("id", ',') WITHIN GROUP (ORDER BY "id")
-          FROM SISEDO.MEETINGV00_VW mtg2 JOIN SISEDO.CLASSSECTIONV00_VW sec3 ON (
-            mtg2."location-code" = mtg."location-code" AND
-            mtg2."meetsDays" = mtg."meetsDays" AND
-            mtg2."startTime" = mtg."startTime" AND
-            mtg2."endTime" = mtg."endTime" AND
-            mtg2."startDate" = mtg."startDate" AND
-            mtg2."endDate" = mtg."endDate" AND
-            mtg2."cs-course-id" = sec3."cs-course-id" AND
-            mtg2."term-id" = sec3."term-id" AND
-            mtg2."session-id" = sec3."session-id" AND
-            mtg2."offeringNumber" = sec3."offeringNumber" AND
-            mtg2."sectionNumber" = sec3."sectionNumber")
-          )
-        END AS co_scheduled_ccns
-      FROM
-        SISEDO.CLASSSECTIONV00_VW sec
-        LEFT OUTER JOIN SISEDO.MEETINGV00_VW mtg ON (
-          mtg."cs-course-id" = sec."cs-course-id" AND
-          mtg."term-id" = sec."term-id" AND
-          mtg."session-id" = sec."session-id" AND
-          mtg."offeringNumber" = sec."offeringNumber" AND
-          mtg."sectionNumber" = sec."sectionNumber")
-        LEFT OUTER JOIN SISEDO.ASSIGNEDINSTRUCTORV00_VW instr ON (
-          instr."cs-course-id" = sec."cs-course-id" AND
-          instr."term-id" = sec."term-id" AND
-          instr."session-id" = sec."session-id" AND
-          instr."offeringNumber" = sec."offeringNumber" AND
-          instr."number" = sec."sectionNumber")
-        LEFT OUTER JOIN SISEDO.DISPLAYNAMEXLAT_MVW xlat ON (
-          xlat."classDisplayName" = sec."displayName")
-        LEFT OUTER JOIN SISEDO.API_COURSEV00_VW crs ON (
-          xlat."courseDisplayName" = crs."displayName"
-          AND crs."status-code" = 'ACTIVE')
-        WHERE
-          sec."term-id" = '#{term_id}'
-          AND #{filter_clause}
-          AND sec."status-code" IN ('A','S')
+            sec."term-id" = '#{term_id}'
+            AND #{filter_clause}
+            AND sec."status-code" IN ('A','S')
+      )
+      GROUP BY section_id, primary, instruction_format, section_num, course_display_name,
+        ldap_uid, sis_id, first_name, last_name, role_code, email_address, course_title_short, enrollment_count,
+        affiliations, cross_listed_ccns, co_scheduled_ccns
       SQL
     end
 
