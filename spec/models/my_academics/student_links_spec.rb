96bcf8eacd310980be243ede540e0dcfb150aec3
Jason Miller <jason@redconfetti.com>
2017-03-03 15:38:19 -0800

    SISRP-30588 - Applied stubbing to MyAcademics::StudentLinks and MyAcademics::AdvisorLinks spec

--- spec/models/my_academics/student_links_spec.rb
+++ spec/models/my_academics/student_links_spec.rb
@@ -1,7 +1,21 @@
 describe MyAcademics::StudentLinks do
   let(:uid) { random_id }
   let(:user_cs_id) { random_id }
-  let(:fake) { true }
+  let(:tcReportLink) do
+    {
+      link: {
+        name: 'Transfer Credit Report',
+        url: 'https://bcswebqat.is.berkeley.edu/psp/bcsqat/EMPLOYEE/HRMS/c/CSU_DA_TRN_CDT_STD.CSU_DA_TRN_CDT_STD.GBL'
+      }
+    }
+  end
+
+  before do
+    # stub CS Link proxy responses
+    fake_cs_link_proxy = double
+    allow(fake_cs_link_proxy).to receive(:get_url).with('UC_CX_XFER_CREDIT_REPORT_STDNT', {}).and_return(tcReportLink)
+    allow(CampusSolutions::Link).to receive(:new).and_return(fake_cs_link_proxy)
+  end
 
   subject do
     {}.tap {|x| MyAcademics::StudentLinks.new(uid).merge(x)}
