3ab317ac46defeb6ea6cda2a85aa0b5712a53174
Jason Miller <jason@redconfetti.com>
2017-05-17 09:49:13 -0700

    SISRP-33211 - Create test script dependent SISEDO views / columns

--- app/models/edo_oracle/connection.rb
+++ app/models/edo_oracle/connection.rb
@@ -5,13 +5,17 @@ module EdoOracle
       Settings.edodb
     end
 
-    def self.safe_query(sql, opts={})
+    def self.query(sql, opts={})
       result = []
       return result if fake?
       use_pooled_connection do
         result = connection.select_all sql
       end
       opts[:do_not_stringify] ? result : stringify_ints!(result)
+    end
+
+    def self.safe_query(sql, opts={})
+      query
     rescue => e
       logger.error "Query failed: #{e.class}: #{e.message}\n #{e.backtrace.join("\n ")}"
       []
---
+++ app/models/edo_oracle/view_checker.rb
@@ -0,0 +1,194 @@
+class EdoOracle::ViewChecker
+
+  VIEW_DEPENDENCIES = [
+    {
+      :id => 'SISEDO.TERM_TBL_VW',
+      :columns => [
+        'STRM',
+        'DESCR',
+        'TERM_BEGIN_DT',
+        'TERM_END_DT',
+      ]
+    },
+    {
+      :id => 'SISEDO.API_COURSEV00_MVW',
+      :columns => [
+        'catalogNumber-formatted',
+        'catalogNumber-number',
+        'catalogNumber-prefix',
+        'catalogNumber-suffix',
+        'cms-id',
+        'classSubjectArea',
+        'displayName',
+        'status-code',
+        'subjectArea',
+        'transcriptTitle',
+        'title',
+      ]
+    },
+    {
+      :id => 'SISEDO.CLASSSECTIONALLV00_MVW',
+      :columns => [
+        'component-code',
+        'cs-course-id',
+        'displayName',
+        'endDate',
+        'finalExam',
+        'id',
+        'maxEnroll',
+        'maxWaitlist',
+        'primary',
+        'primaryAssociatedSectionId',
+        'printInScheduleOfClasses',
+        'sectionNumber',
+        'status-code',
+        'session-id',
+        'startDate',
+        'term-id',
+      ]
+    },
+    {
+      :id => 'SISEDO.DISPLAYNAMEXLAT_MVW',
+      :columns => [
+        'classDisplayName',
+        'courseDisplayName',
+      ]
+    },
+    {
+      :id => 'SISEDO.ENROLLMENTV00_VW',
+      :columns => [
+        'ACAD_CAREER',
+        'CAMPUS_UID',
+        'CLASS_SECTION_ID',
+        'GRADING_BASIS_CODE',
+        'STDNT_ENRL_STATUS_CODE',
+        'STUDENT_ID',
+        'TERM_ID',
+        'UNITS_TAKEN',
+        'WAITLISTPOSITION',
+      ]
+    },
+    {
+      :id => 'SISEDO.CC_ENROLLMENTV00_VW',
+      :columns => [
+        'ACAD_CAREER',
+        'CAMPUS_UID',
+        'CLASS_SECTION_ID',
+        'GRADE_MARK',
+        'GRADE_POINTS',
+        'GRADING_BASIS_CODE',
+        'SESSION_ID',
+        'STDNT_ENRL_STATUS_CODE',
+        'STUDENT_ID',
+        'TERM_ID',
+        'UNITS_TAKEN',
+        'WAITLISTPOSITION',
+      ]
+    },
+    {
+      :id => 'SISEDO.ASSIGNEDINSTRUCTORV00_VW',
+      :columns => [
+        'campus-uid',
+        'cs-course-id',
+        'familyName',
+        'formattedName',
+        'givenName',
+        'gradeRosterAccess',
+        'instructor-id',
+        'number',
+        'offeringNumber',
+        'printInScheduleOfClasses',
+        'role-code',
+        'role-descr',
+        'session-id',
+        'term-id',
+      ]
+    },
+    {
+      :id => 'SISEDO.EXAMV00_VW',
+      :columns => [
+        'cs-course-id',
+        'date',
+        'endTime',
+        'location-descr',
+        'offeringNumber',
+        'session-id',
+        'sectionNumber',
+        'startTime',
+        'term-id',
+        'type-code',
+      ]
+    },
+    {
+      :id => 'SISEDO.MEETINGV00_VW',
+      :columns => [
+        'cs-course-id',
+        'endDate',
+        'endTime',
+        'location-descr',
+        'meetsDays',
+        'offeringNumber',
+        'sectionNumber',
+        'session-id',
+        'startDate',
+        'startTime',
+        'term-id',
+      ]
+    },
+    {
+      :id => 'SISEDO.PERSON_EMAILV00_VW',
+      :columns => [
+        'PERSON_KEY',
+        'EMAIL_PRIMARY',
+      ]
+    },
+    {
+      :id => 'SISEDO.API_COURSEIDENTIFIERSV00_VW',
+      :columns => ['subjectArea']
+    }
+  ]
+
+  def initialize
+    @report = {
+      :successes => [],
+      :errors => []
+    }
+  end
+
+  def perform_checks
+    VIEW_DEPENDENCIES.each do |view|
+      check_view(view)
+    end
+    @report
+  end
+
+  def check_view(view)
+    results = EdoOracle::Queries.query "SELECT #{to_query_columns(view[:columns])} FROM #{view[:id]} WHERE rownum=1"
+    check_results(view[:columns], view[:id], results)
+  rescue => e
+    log_result(:errors, "Failure to query #{view[:id]} - #{e.to_s}")
+  end
+
+  def check_results(column_array, view_name, results)
+    errors = 0
+    column_array.each do |column|
+      if results[0][column.downcase].blank?
+        errors += 1
+        log_result(:errors, "#{column} column value blank in #{view_name} results")
+      end
+    end
+    if errors == 0
+      log_result(:successes, "#{view_name} has no issues")
+    end
+  end
+
+  def log_result(type, message)
+    @report[type].push(message)
+  end
+
+  def to_query_columns(column_names_array)
+    column_names_array.map {|column_name|
+      "\"#{column_name}\""
+    }.join(',')
+  end
+end
