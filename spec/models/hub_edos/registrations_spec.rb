fdf8b6dff9a8ff866cd642e00e25ec7a4a4af546
David Cheng <davidlimcheng@gmail.com>
2017-06-14 17:09:07 -0700

    SISRP-34118 - Rspec Unit Tests for Refactored/Renewed CNP Messaging

--- spec/models/hub_edos/registrations_spec.rb
+++ spec/models/hub_edos/registrations_spec.rb
@@ -8,8 +8,8 @@ describe HubEdos::Registrations do
 
     it 'returns data with the expected structure' do
       expect(subject[:feed]['registrations']).to be
-      expect(subject[:feed]['registrations'][0]['academicCareer']['code']).to eq('LAW')
-      expect(subject[:feed]['registrations'].size).to eq(1)
+      expect(subject[:feed]['registrations'][0]['academicCareer']['code']).to eq('GRAD')
+      expect(subject[:feed]['registrations'].size).to eq(17)
     end
   end
 
--- spec/models/my_registrations/statuses_spec.rb
+++ spec/models/my_registrations/statuses_spec.rb
@@ -1,50 +1,47 @@
 describe MyRegistrations::Statuses do
   let(:model) { MyRegistrations::Statuses.new('61889') }
-  # TODO: Update the registrations to reflect actual data returned by EDO Hub
-  let(:hub_student_registrations) do
-    {
-      "affiliations" => [
-        {"type"=>{"code"=>"STUDENT", "description"=>""}, "detail"=>"", "status"=>{"code"=>"ACT", "description"=>"Active"}, "fromDate"=>"2015-12-17"},
-        {"type"=>{"code"=>"UNDERGRAD", "description"=>"Undergraduate Student"}, "detail"=>"Active", "status"=>{"code"=>"ACT", "description"=>"Active"}, "fromDate"=>"2015-12-17"}
-      ],
-      "registrations" => [
-        {
-            "term" => {
-            "id" => 2168,
-            "name" => "2016 Fall",
-            "academicYear" => "2017"
-          }
-        },
-        {
-          "term" => {
-            "id" => 2171,
-            "name" => "2017 Spring",
-            "academicYear" => "2018"
-          }
-        }
-      ]
-    }
-  end
+  let(:hub_student_registrations) { HubEdos::Registrations.new(user_id: 61889, fake: true).get }
+  let(:hub_student_attributes) { HubEdos::StudentAttributes.new(user_id: 61889, fake: true).get }
   before(:each) do
     allow(model).to receive(Settings.terms.legacy_cutoff).and_return 'summer-2016'
-    allow(model).to receive(:get_registrations).and_return(hub_student_registrations)
+    allow(model).to receive(:registrations).and_return(hub_student_registrations[:feed]['registrations'])
+    allow(model).to receive(:studentAttributes).and_return(hub_student_attributes[:feed]['student']['studentAttributes'])
   end
 
   context 'fully populated berkeley terms' do
     before do
       allow(model).to receive(:get_terms).and_return ({
-        current: {id: 2165, name: 'Summer 2016'},
-        running: {id: 2165, name: 'Summer 2016'},
-        sis_current_term: {id: 2168, name: 'Fall 2016'},
-        next: {id: 2168, name: 'Fall 2016'},
-        future: {id: 2171, name: 'Spring 2017'}
+        current: {id: '1932', name: 'Spring 1993', classesStart: Date.parse('1993-05-22T00:00:00.000-07:00'), end: Date.parse('1993-08-11T23:59:59.000-07:00'), endDropAdd: false},
+        running: {id: '1952', name: 'Spring 1995', classesStart: Date.parse('1995-05-22T00:00:00.000-07:00'), end: Date.parse('1995-08-11T23:59:59.000-07:00'), endDropAdd: false},
+        sis_current_term: {id: '2175', name: 'Summer 2017', classesStart: Date.parse('2017-05-22T00:00:00.000-07:00'), end: Date.parse('2017-08-11T23:59:59.000-07:00'), endDropAdd: false},
+        next: {id: '2178', name: 'Fall 2017', classesStart: Date.parse('2017-08-23T00:00:00.000-07:00'), end: Date.parse('2017-08-11T23:59:59.000-07:00'), endDropAdd: Date.parse('2017-09-22')},
+        future: {id: '2182', name: 'Spring 2018', classesStart: Date.parse('2018-01-16T00:00:00.000-08:00'), end: Date.parse('2018-05-11T23:59:59.000-07:00'), endDropAdd: nil}
       })
     end
+    subject { model.get_feed_internal }
+
+    it 'adds the appropriate term flags' do
+      current = model.send(:set_term_flags, subject[:terms][:current])
+      running = model.send(:set_term_flags, subject[:terms][:running])
+      expect(current[:pastClassesStart]).to eq true
+      expect(running[:pastEndOfInstruction]).to eq true
+      expect(subject[:registrations]['2175'][:isSummer]).to eq true
+      expect(subject[:registrations]['2182'][:isSummer]).to eq false
+    end
+
+    it 'adds the appropriate visibility flags' do
+      expect(subject[:registrations]['2182'][:showRegStatus]).to eq true
+      expect(subject[:registrations]['2182'][:showCnp]).to eq false
+    end
+
     it 'returns all matched terms' do
-      matched_terms = model.get_feed_internal
-      expect(matched_terms[:registrations][2165][0][:isLegacy]).to eq true
-      expect(matched_terms[:registrations][2168][0]["term"]["id"]).to eq 2168
-      expect(matched_terms[:registrations][2171][0]["term"]["id"]).to eq 2171
+      expect(subject[:registrations]['1932']['term']['id']).to eq '1932'
+      expect(subject[:registrations]['1952']['term']['id']).to eq '1952'
+      expect(subject[:registrations]['2182']['term']['id']).to eq '2182'
+    end
+
+    it 'matches positive indicators to the correct term' do
+      expect(subject[:registrations]['2182'][:positiveIndicators][0]['type']['code']).to eq '+S09'
     end
   end
 
@@ -54,36 +51,25 @@ describe MyRegistrations::Statuses do
         current: nil,
         running: nil,
         sis_current_term: nil,
-        next: {id: 2168, name: 'Fall 2016'},
-        future: {id: 2171, name: 'Spring 2017'}
+        next: {id: '2178', name: 'Fall 2017', classesStart: '2017-08-23T00:00:00.000-07:00', end: '2017-08-11T23:59:59.000-07:00', endDropAdd: '2017-09-22'},
+        future: {id: '2182', name: 'Spring 2018', classesStart: '2018-01-16T00:00:00.000-08:00', end: '2018-05-11T23:59:59.000-07:00', endDropAdd: nil}
       })
     end
+    subject { model.get_feed_internal }
 
     context 'registrations present' do
-      it 'returns affiliations' do
-        matched_terms = model.get_feed_internal
-        expect(matched_terms[:affiliations]).to be_an_instance_of Array
-        expect(matched_terms[:affiliations].count).to eq 2
-      end
       it 'returns all matched terms' do
-        matched_terms = model.get_feed_internal
-        expect(matched_terms[:registrations][2165]).to be_nil
-        expect(matched_terms[:registrations][2168][0]["term"]["id"]).to eq 2168
-        expect(matched_terms[:registrations][2171][0]["term"]["id"]).to eq 2171
+        expect(subject[:registrations]['2182']['term']['id']).to eq '2182'
       end
     end
 
     context 'registrations not present' do
-      before {
-        allow(model).to receive(:get_registrations).and_return ({})
-      }
-      it 'returns no affiliations' do
-        matched_terms = model.get_feed_internal
-        expect(matched_terms[:affiliations]).to eq []
+      before do
+        allow(model).to receive(:registrations).and_return ({})
       end
+
       it 'returns no matched terms' do
-        matched_terms = model.get_feed_internal
-        expect(matched_terms[:registrations]).to eq Hash.new
+        expect(subject[:registrations]).to eq ({})
       end
     end
   end
@@ -98,11 +84,10 @@ describe MyRegistrations::Statuses do
         future: nil
       })
     end
+    subject { model.get_feed_internal }
+
     it 'returns no matched terms' do
-      matched_terms = model.get_feed_internal
-      expect(matched_terms[:registrations][2165]).to be_nil
-      expect(matched_terms[:registrations][2168]).to be_nil
-      expect(matched_terms[:registrations][2171]).to be_nil
+      expect(subject[:registrations]).to eq ({})
     end
   end
 
