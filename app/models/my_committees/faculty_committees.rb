b59392fb742aaff91a09a3967c9bb192e48532c0
lyttam <annemlyttle@gmail.com>
2017-09-19 13:35:21 -0700

    SISRP-36097 - Removes milestone attempts and status from Student Committees card

--- app/models/my_committees/faculty_committees.rb
+++ app/models/my_committees/faculty_committees.rb
@@ -40,8 +40,54 @@ module MyCommittees
     end
 
     def merge_faculty_specific_data(cs_committee, faculty_committee)
+      faculty_committee.merge! milestoneAttempts: parse_cs_qualifying_exam_attempts(cs_committee)
       faculty_committee.merge! student: parse_cs_committee_student(cs_committee)
       faculty_committee.merge! parse_cs_faculty_committee_svc(cs_committee)
+    end
+
+    def set_committee_status(cs_committee, committee)
+      if qualifying_exam?(cs_committee)
+        committee[:statusIcon] = determine_qualifying_exam_status_icon(committee)
+        committee[:statusMessage] = determine_qualifying_exam_status_message(cs_committee)
+      elsif !is_active?(cs_committee) && cs_committee[:studentFilingDate]
+        committee[:statusIcon] = STATUS_ICON_SUCCESS
+        committee[:statusMessage] = "Filing Date: #{format_date(cs_committee[:studentFilingDate])}"
+      elsif (advanced_date = determine_advanced_date(cs_committee))
+        committee[:statusMessage] = "Advanced: #{format_date(advanced_date)}"
+      end
+    end
+
+    def determine_qualifying_exam_status_icon(committee)
+      latest_attempt = committee.try(:[], :milestoneAttempts).try(:first)
+      return '' unless latest_attempt
+      if latest_attempt.try(:[], :result) == Berkeley::GraduateMilestones::QE_RESULTS_STATUS_PASSED
+        STATUS_ICON_SUCCESS
+      elsif latest_attempt.try(:[], :sequenceNumber) === 1
+        STATUS_ICON_WARN
+      else
+        STATUS_ICON_FAIL
+      end
+    end
+
+    def determine_advanced_date(cs_committee)
+      if (advanced_date = cs_committee[:studentAdvancedDate]).blank?
+        advanced_date = latest_milestone_attempt(cs_committee).try(:[], :attemptDate)
+      end
+      advanced_date
+    end
+
+    def latest_milestone_attempt(cs_committee)
+      cs_committee.try(:[], :studentApprovalMilestoneAttempts).try(:sort_by!) do |attempt|
+        attempt.try(:[], :attemptNbr)
+      end.try(:last)
+    end
+
+    def parse_cs_qualifying_exam_attempts(cs_committee)
+      qualifying_exam_attempts = []
+      if qualifying_exam?(cs_committee)
+        qualifying_exam_attempts = parse_cs_milestone_attempts(cs_committee)
+      end
+      qualifying_exam_attempts
     end
 
     def parse_cs_milestone_attempts(cs_committee)
@@ -51,6 +97,16 @@ module MyCommittees
       attempts.try(:sort_by) do |attempt|
         attempt[:sequenceNumber]
       end.try(:reverse)
+    end
+
+    def parse_cs_milestone_attempt(cs_milestone_attempt)
+      milestone_attempt = {
+        sequenceNumber: cs_milestone_attempt[:attemptNbr].to_i,
+        date: format_date(cs_milestone_attempt[:attemptDate]),
+        result: Berkeley::GraduateMilestones.get_status(cs_milestone_attempt[:attemptStatus], Berkeley::GraduateMilestones::QE_RESULTS_MILESTONE)
+      }
+      milestone_attempt[:display] = format_milestone_attempt(milestone_attempt)
+      milestone_attempt
     end
 
     def format_milestone_attempt(milestone_attempt)
