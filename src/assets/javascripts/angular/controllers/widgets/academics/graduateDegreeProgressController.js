543518a1c9108ef170cd2844a402412143224296
David Lim Cheng <davidlimcheng@gmail.com>
2018-04-25 13:58:56 -0700

    SISRP-38416 - LAW: Remove Degree progress message in CalCentral

--- src/assets/javascripts/angular/controllers/widgets/academics/graduateDegreeProgressController.js
+++ src/assets/javascripts/angular/controllers/widgets/academics/graduateDegreeProgressController.js
@@ -21,7 +21,7 @@ angular.module('calcentral.controllers').controller('GraduateDegreeProgressContr
       }
     ).finally(
       function() {
-        var isHigherDegreeStudent = apiService.user.profile.academicRoles.grad || apiService.user.profile.academicRoles.law;
+        var isHigherDegreeStudent = apiService.user.profile.academicRoles.grad || (apiService.user.profile.academicRoles.law && apiService.user.profile.academicRoles.lawJspJsd);
         var isExStudentWithMilestones = apiService.user.profile.roles.exStudent && $scope.degreeProgress.graduate.progresses.length;
         $scope.degreeProgress.graduate.showCard = apiService.user.profile.features.csDegreeProgressGradStudent && (isHigherDegreeStudent || isExStudentWithMilestones);
         $scope.degreeProgress.graduate.isLoading = false;
