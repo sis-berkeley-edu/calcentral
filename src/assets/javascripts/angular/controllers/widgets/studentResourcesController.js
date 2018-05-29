2bcae91e297ab3da0200349d72676267effc9bf7
Jason Miller <jason@redconfetti.com>
2018-05-29 16:12:54 -0700

    SISRP-40143 - Some of the Student resources link are not showing

--- src/assets/javascripts/angular/controllers/widgets/studentResourcesController.js
+++ src/assets/javascripts/angular/controllers/widgets/studentResourcesController.js
@@ -22,7 +22,7 @@ angular.module('calcentral.controllers').controller('StudentResourcesController'
     $scope.isGraduateStudent = apiService.user.profile.roles.graduate;
     $scope.isUndergraduate = apiService.user.profile.roles.undergrad;
     $scope.isSummerVisitor = apiService.user.profile.academicRoles.current.summerVisitor;
-    $scope.isJdLlmOnly = (apiService.user.profile.academicRoles.current.lawJdLlm && !apiService.user.profile.academicRoles.current.lawJspJsd && !apiService.user.profile.academicRoles.current.grad);
+    $scope.isJdLlmOnly = ((apiService.user.profile.academicRoles.current.lawJdLlm || apiService.user.profile.academicRoles.current.lawJdCdp) && !apiService.user.profile.academicRoles.current.lawJspJsd && !apiService.user.profile.academicRoles.current.grad);
     $scope.isLawVisiting = (apiService.user.profile.academicRoles.current.lawVisiting && !apiService.user.profile.academicRoles.current.grad);
     $scope.isNonDegreeSeekingSummerVisitor = academicsService.isNonDegreeSeekingSummerVisitor(apiService.user.profile.academicRoles);
   };
