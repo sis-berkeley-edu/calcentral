fdfa49df8916b1039e6ac45665785a37e70fdd90
David Lim Cheng <davidlimcheng@gmail.com>
2018-07-25 11:11:13 -0700

    SISRP-41513 - Transfer Credit card is missing on My Academics Page

--- src/assets/javascripts/angular/controllers/widgets/academics/transferCreditController.js
+++ src/assets/javascripts/angular/controllers/widgets/academics/transferCreditController.js
@@ -6,6 +6,7 @@ var _ = require('lodash');
 angular.module('calcentral.controllers').controller('TransferCreditController', function(transferCreditFactory, $scope) {
 
   $scope.transferCredits = {
+    hasUnits: false,
     isLoading: true,
     law: {
       show: true
@@ -25,6 +26,7 @@ angular.module('calcentral.controllers').controller('TransferCreditController', 
     transferCreditFactory.getTransferCredit()
       .then(function(response) {
         var transferCredits = _.get(response, 'data');
+        $scope.transferCredits.hasUnits = _.get(transferCredits, 'hasUnits');
         angular.extend($scope.transferCredits.law, transferCredits.law);
         angular.extend($scope.transferCredits.graduate, transferCredits.graduate);
         angular.extend($scope.transferCredits.undergraduate, transferCredits.undergraduate);
