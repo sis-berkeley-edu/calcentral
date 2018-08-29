dec0aa6b961076325da1f901b3bbbc85aa40f67c
Ray Davis <raydavis@berkeley.edu>
2018-08-29 14:08:13 -0700

    CLC-6896 Make Junction's data available to multiple Nessie environments

--- app/models/data_loch/s3.rb
+++ app/models/data_loch/s3.rb
@@ -2,28 +2,37 @@ module DataLoch
   class S3
     include ClassLogger
 
-    def initialize
-      @settings = Settings.data_loch
+    def initialize(target=nil)
+      settings = Settings.data_loch
+      if target
+        s3_config = settings.targets.find {|c| c.name == target}
+        raise ArgumentError, "Could not find data_loch target #{target}" unless s3_config
+      else
+        # TODO Remove support for old one-S3-target-only configurations in next release.
+        s3_config = settings
+      end
+      @bucket = s3_config.bucket
+      @prefix = s3_config.prefix
       @resource = Aws::S3::Resource.new(
-        credentials: Aws::Credentials.new(@settings.aws_key, @settings.aws_secret),
-        region: @settings.aws_region
+        credentials: Aws::Credentials.new(s3_config.aws_key, s3_config.aws_secret),
+        region: s3_config.aws_region
       )
     end
 
     def upload(subfolder, local_path, is_historical=false)
       if is_historical
-        key = "#{@settings.prefix}/historical/#{subfolder}/#{File.basename local_path}"
+        key = "#{@prefix}/historical/#{subfolder}/#{File.basename local_path}"
       else
         today = (Settings.terms.fake_now || DateTime.now).in_time_zone.strftime('%Y-%m-%d')
         digest = Digest::MD5.hexdigest today
-        key = "#{@settings.prefix}/daily/#{digest}-#{today}/#{subfolder}/#{File.basename local_path}"
+        key = "#{@prefix}/daily/#{digest}-#{today}/#{subfolder}/#{File.basename local_path}"
       end
       begin
-        @resource.bucket(@settings.bucket).object(key).upload_file local_path, server_side_encryption: 'AES256'
-        logger.info("S3 upload complete (bucket=#{@settings.bucket}, key=#{key}")
+        @resource.bucket(@bucket).object(key).upload_file local_path, server_side_encryption: 'AES256'
+        logger.info("S3 upload complete (bucket=#{@bucket}, key=#{key}")
         key
       rescue => e
-        logger.error("Error on S3 upload (bucket=#{@settings.bucket}, key=#{key}: #{e.message}")
+        logger.error("Error on S3 upload (bucket=#{@bucket}, key=#{key}: #{e.message}")
         nil
       end
     end
