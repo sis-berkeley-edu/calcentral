25cb4b2506c1d44d3d02eb250afd5a0dae25231a
Jason Miller <jason@redconfetti.com>
2018-08-29 12:13:51 -0700

    SISRP-39065 - AA: UX Dev - Advisor Student Lookup

---
+++ src/assets/javascripts/angular/services/emailService.js
@@ -0,0 +1,19 @@
+/* jshint camelcase: false */
+'use strict';
+
+var angular = require('angular');
+
+angular.module('calcentral.services').factory('emailService', function() {
+  /**
+   * Returns link to gMail compose with TO address specified
+   * @param {String} toAddress 'TO' address string
+   */
+  var bmailLink = function(toAddress) {
+    var urlEncodedToAddress = encodeURIComponent(toAddress);
+    return 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=' + urlEncodedToAddress;
+  };
+
+  return {
+    bmailLink: bmailLink
+  };
+});
