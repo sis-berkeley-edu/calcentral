8aecb1629b3bed6aecefa83851a53da72a32c79a
Jason Miller <jason@redconfetti.com>
2018-09-06 17:44:09 -0700

    SISRP-41780 - CalCentral Cal 1 Card Meal Plan Update

--- src/assets/javascripts/angular/factories/cal1cardFactory.js
+++ src/assets/javascripts/angular/factories/cal1cardFactory.js
@@ -8,6 +8,8 @@ var angular = require('angular');
 angular.module('calcentral.factories').factory('cal1CardFactory', function(apiService) {
   var url = '/api/my/cal1card';
   // var url = '/dummy/json/cal1card.json';
+  // var url = '/dummy/json/cal1card-lost.json';
+  // var url = '/dummy/json/cal1card-found.json';
 
   var getCal1Card = function(options) {
     return apiService.http.request(options, url);
