5ac2f0f6ac839014bf79f0d2fb4e467dcdaa29d2
Anne Lyttle <alyttle@berkeley.edu>
2018-09-20 16:27:57 -0700

    SISRP-42629: adds EMPLID param to Shopping Sheet link

--- spec/models/edo_oracle/financial_aid/queries_spec.rb
+++ spec/models/edo_oracle/financial_aid/queries_spec.rb
@@ -125,7 +125,7 @@ describe EdoOracle::FinancialAid::Queries do
     it_behaves_like 'a successful query that returns one result'
 
     it 'returns the expected result' do
-      expect(subject).to have_keys(%w(uc_cost_attendance uc_gift_aid_waiver uc_net_cost uc_funding_offered uc_gift_aid_out uc_grants_schol uc_waivers_oth uc_fee_waivers uc_loans_wrk_study uc_loans uc_work_study sfa_ss_group))
+      expect(subject).to have_keys(%w(student_id uc_cost_attendance uc_gift_aid_waiver uc_net_cost uc_funding_offered uc_gift_aid_out uc_grants_schol uc_waivers_oth uc_fee_waivers uc_loans_wrk_study uc_loans uc_work_study sfa_ss_group))
     end
   end
 end
