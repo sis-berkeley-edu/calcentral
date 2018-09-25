13565ba3e24a533fbdebfe3fc1c97952e5b59fe1
Anne Lyttle <alyttle@berkeley.edu>
2018-09-25 08:49:39 -0700

    Revert "SISRP-39902: Adds edit button to demographic info"
    This reverts commit 042e6bc564ea3e8dcdc7049a33a9601a8147a65d.

--- src/assets/javascripts/angular/controllers/widgets/profile/demographicController.js
+++ src/assets/javascripts/angular/controllers/widgets/profile/demographicController.js
@@ -11,16 +11,11 @@ angular.module('calcentral.controllers').controller('DemographicController', fun
     isErrored: false
   };
 
-  var parseDemographicInformation = function(response) {
-    $scope.demographicInformation.isErrored = _.get(response, 'data.errored');
-    $scope.demographicInformation.editLink = _.get(response, 'data.feed.links.editDemographicInformation');
-  };
-
   var loadInformation = function() {
     profileFactory.getPerson()
-    .then(parseDemographicInformation)
-    .finally(function() {
+    .then(function(response) {
       $scope.demographicInformation.isLoading = false;
+      $scope.demographicInformation.isErrored = _.get(response, 'data.errored');
     });
   };
 
