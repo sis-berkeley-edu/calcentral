a536bc2a96879e398207d5b125f227474126e2f8
Anne Lyttle <alyttle@berkeley.edu>
2018-10-11 16:53:48 -0700

    Revert "Revert "SISRP-39903: Replaces preferred name edit with link to CS page""
    This reverts commit 40b5f268e55230004deae968778bc23db00dd57c.

---
+++ src/assets/javascripts/angular/controllers/widgets/profilePopoverController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var angular = require('angular');
+var _ = require('lodash');
+
+angular.module('calcentral.controllers').controller('ProfilePopoverController', function(csLinkFactory, $scope) {
+  $scope.profilePopover = {
+    isLoading: true
+  };
+
+  var loadLink = function() {
+    csLinkFactory.getLink({
+      urlId: 'UC_CX_PROFILE'
+    }).then(function(response) {
+      $scope.profilePopover.link = _.get(response, 'data.link');
+      $scope.profilePopover.isLoading = false;
+    });
+  };
+
+  loadLink();
+});
