bd6cae091a9cc1a2922bf00abeeb31669cc32e3f
Nader Kutub <naderk@berkeley.edu>
2018-11-14 10:21:50 -0800

    SISRP-42013 - Academic Status updates (UX Dev)
    update javascript
    
    fix js error
    
    fix complexity

--- src/assets/javascripts/angular/controllers/pages/academicsController.js
+++ src/assets/javascripts/angular/controllers/pages/academicsController.js
@@ -235,10 +235,17 @@ angular.module('calcentral.controllers').controller('AcademicsController', funct
     $scope.isNonDegreeSeekingSummerVisitor = isNonDegreeSeekingSummerVisitor;
     $scope.showStatusAndBlocks = !$scope.filteredForDelegate &&
                                  ($scope.hasRegStatus || $scope.numberOfHolds || $scope.hasStandingAlert);
+    filterStanding();
     $scope.showAdvising = !$scope.filteredForDelegate && apiService.user.profile.features.advising && apiService.user.profile.roles.student && isMbaJdOrNotLaw() && !isNonDegreeSeekingSummerVisitor;
     $scope.showProfileMessage = (!$scope.isAcademicInfoAvailable || !$scope.collegeAndLevel || _.isEmpty($scope.collegeAndLevel.careers));
     $scope.showResidency = apiService.user.profile.roles.student && academicsService.showResidency(apiService.user.profile.academicRoles.current);
     $scope.showGpaSection = academicsService.showGpa($scope.gpaUnits.gpa);
+  };
+
+  var filterStanding = function() {
+    var standingVisibleForStudent = $scope.hasStandingAlert || apiService.user.profile.academicRoles.current.ugrd;
+    var standingVisibleForAdvisor = apiService.user.profile.roles.advisor && ($scope.targetUser.academicRoles.current.ugrd || $scope.hasStandingAlert);
+    $scope.standingIsVisible = (standingVisibleForStudent || standingVisibleForAdvisor) && !$scope.targetUser.academicRoles.current.ugrdNonDegree;
   };
 
   /**
@@ -263,7 +270,7 @@ angular.module('calcentral.controllers').controller('AcademicsController', funct
       if (apiService.user.profile.roles.student || apiService.user.profile.roles.applicant || apiService.user.profile.roles.exStudent || apiService.user.profile.roles.concurrentEnrollmentStudent) {
         requests.push(loadNumberOfHolds());
       }
-      if (apiService.user.profile.academicRoles.current.ugrd && !apiService.user.profile.academicRoles.current.ugrdNonDegree) {
+      if (!apiService.user.profile.academicRoles.current.grad || !apiService.user.profile.academicRoles.current.law && !apiService.user.profile.academicRoles.current.ugrdNonDegree && !apiService.user.profile.academicRoles.current.concurrent) {
         requests.push(loadHasStanding());
       }
       $q.all(requests).then(filterWidgets);
