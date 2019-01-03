e0385f6a1737569788ee95ecb81e06a70f61667c
Marc Pechaitis <marcpechaitis@gmail.com>
2019-01-02 20:17:02 -0700

    SISRP-41873 Removes FinaidApprovalsController and refactors TermsAndConditionsController & Title4Controller

--- src/assets/javascripts/angular/factories/finaidFactory.js
+++ src/assets/javascripts/angular/factories/finaidFactory.js
@@ -3,7 +3,7 @@
 /**
  * Financial Aid Factory
  */
-angular.module('calcentral.factories').factory('finaidFactory', function(apiService, $http) {
+angular.module('calcentral.factories').factory('finaidFactory', function(apiService) {
   var urlAwards = '/api/campus_solutions/financial_aid_funding_sources';
   // var urlAwards = '/dummy/json/financial_aid_funding_sources.json';
 
@@ -19,9 +19,6 @@ angular.module('calcentral.factories').factory('finaidFactory', function(apiServ
   var urlFinancialAidSummary = 'api/my/financial_aid_summary';
   var urlSummary = '/api/my/aid_years';
   // var urlSummary = '/dummy/json/finaid_summary.json';
-
-  var urlPostTC = '/api/campus_solutions/terms_and_conditions';
-  var urlPostT4 = '/api/campus_solutions/title4';
 
   var getAwards = function(options) {
     return apiService.http.request(options, urlAwards + '?aid_year=' + options.finaidYearId);
@@ -59,18 +56,6 @@ angular.module('calcentral.factories').factory('finaidFactory', function(apiServ
     return apiService.http.request(options, urlSummary);
   };
 
-  var postTCResponse = function(finaidYearId, response) {
-    return $http.post(urlPostTC, {
-      aidYear: finaidYearId,
-      response: response
-    });
-  };
-  var postT4Response = function(response) {
-    return $http.post(urlPostT4, {
-      response: response
-    });
-  };
-
   return {
     getAwards: getAwards,
     getAwardsTerm: getAwardsTerm,
@@ -79,8 +64,6 @@ angular.module('calcentral.factories').factory('finaidFactory', function(apiServ
     getAwardComparePrior: getAwardComparePrior,
     getFinaidYearInfo: getFinaidYearInfo,
     getFinancialAidSummary: getFinancialAidSummary,
-    getSummary: getSummary,
-    postTCResponse: postTCResponse,
-    postT4Response: postT4Response
+    getSummary: getSummary
   };
 });
