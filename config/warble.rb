9a0228d078341442b9bbc104b7bb7cb02d035c8a
Brandon Wright <brandon@developish.com>
2019-01-16 12:33:06 -0700

    Specify warfile name for bamboo build purposes

--- config/warble.rb
+++ config/warble.rb
@@ -2,4 +2,5 @@ Warbler::Config.new do |config|
   config.dirs = %w(app bin config db lib log public script vendor versions tmp)
   config.init_contents << StringIO.new("\nGem.clear_paths\nGem.path\n")
   config.jrubyc_options = "--javac"
+  config.war_name = "calcentral"
 end
