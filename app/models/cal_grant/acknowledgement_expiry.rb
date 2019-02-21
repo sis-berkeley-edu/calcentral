cfb78ca1c2ccda6201a4e00c95fdf7e9d0a83d55
Brandon Wright <brandon@developish.com>
2019-02-21 14:13:01 -0700

    SISRP-44030: Allow expiration at the server with refresh=true

--- app/models/cal_grant/acknowledgement_expiry.rb
+++ app/models/cal_grant/acknowledgement_expiry.rb
@@ -2,6 +2,8 @@ module CalGrant
   module AcknowledgementExpiry
     def self.expire(uid=nil)
       CalGrant::Acknowledgement.expire uid
+      MyAcademics::MyHolds.expire uid
+      MyAcademics::MyAcademicStatus.expire uid
     end
   end
 end
