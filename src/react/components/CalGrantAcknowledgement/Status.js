927544020aac3fd49b4e8c718fcfe1dbe9acbfd6
Brandon Wright <brandon@developish.com>
2019-02-22 10:21:54 -0700

    SISRP-44030: Remove 'View Completed' link from acknowledgement status

--- src/react/components/CalGrantAcknowledgement/Status.js
+++ src/react/components/CalGrantAcknowledgement/Status.js
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types';
 
 import { react2angular } from 'react2angular';
 
-import APILink from '../APILink';
 import HoldListItem from './HoldListItem';
 import { isComplete, isIncomplete } from './states';
 import { forTermId } from '../../helpers/terms';
 
 const propTypes = {
   acknowledgements: PropTypes.array,
-  viewAllLink: PropTypes.object,
   termId: PropTypes.string,
   holds: PropTypes.array
 };
 
 const CALGRANT_SERVICE_INDICATOR_TYPE_CODE = 'F06';
 
-const CalGrantAcknowledgementStatus = ({ acknowledgements, termId, viewAllLink, holds }) => {
+const CalGrantAcknowledgementStatus = ({ acknowledgements, termId, holds }) => {
   const acknowledgement = forTermId(acknowledgements, termId);
 
   if (acknowledgement) {
@@ -26,9 +24,7 @@ const CalGrantAcknowledgementStatus = ({ acknowledgements, termId, viewAllLink, 
         <HoldListItem
           title={`${acknowledgement.link.name} Complete`}
           state="green"
-        >
-          <APILink {...viewAllLink} />
-        </HoldListItem>
+        />
       );
     } else if (isIncomplete(acknowledgement)) {
       const serviceIndicator = holds.filter(hold => {
@@ -42,8 +38,7 @@ const CalGrantAcknowledgementStatus = ({ acknowledgements, termId, viewAllLink, 
             title={acknowledgement.link.name}
             actionLink={acknowledgement.link}
           >
-            {serviceIndicator.reason.formalDescription}&nbsp;
-            <APILink {...viewAllLink} />
+            {serviceIndicator.reason.formalDescription}
           </HoldListItem>
         );
       } else {
