26f5ead9479d4c0ae203ffb791058fe8ac69a52a
Jason Miller <jason@redconfetti.com>
2019-05-02 14:03:14 -0700

    SISRP-45788 - Removes ETS and Campus Link tables not being migrated to Oracle

---
+++ db/migrate/20190502203150_remove_ets_tables.rb
@@ -0,0 +1,61 @@
+class RemoveEtsTables < ActiveRecord::Migration
+  def up
+    drop_table :canvas_site_mailing_list_members
+    drop_table :canvas_site_mailing_lists
+    drop_table :canvas_synchronization
+    drop_table :oec_course_codes
+    drop_table :webcast_course_site_log
+  end
+
+  def down
+    create_table "canvas_site_mailing_list_members", force: :cascade do |t|
+      t.integer  "mailing_list_id",                             null: false
+      t.string   "first_name",      limit: 255
+      t.string   "last_name",       limit: 255
+      t.string   "email_address",   limit: 255,                 null: false
+      t.boolean  "can_send",                    default: false, null: false
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+    add_index "canvas_site_mailing_list_members", ["mailing_list_id", "email_address"], name: "mailing_list_membership_index", unique: true, using: :btree
+
+    create_table "canvas_site_mailing_lists", force: :cascade do |t|
+      t.string   "canvas_site_id",         limit: 255
+      t.string   "canvas_site_name",       limit: 255
+      t.string   "list_name",              limit: 255
+      t.string   "state",                  limit: 255
+      t.datetime "populated_at"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "members_count"
+      t.integer  "populate_add_errors"
+      t.integer  "populate_remove_errors"
+      t.string   "type",                   limit: 255
+    end
+    add_index "canvas_site_mailing_lists", ["canvas_site_id"], name: "index_canvas_site_mailing_lists_on_canvas_site_id", unique: true, using: :btree
+
+    create_table "canvas_synchronization", force: :cascade do |t|
+      t.datetime "last_guest_user_sync"
+      t.datetime "latest_term_enrollment_csv_set"
+    end
+
+    create_table "oec_course_codes", force: :cascade do |t|
+      t.string   "dept_name",      limit: 255, null: false
+      t.string   "catalog_id",     limit: 255, null: false
+      t.string   "dept_code",      limit: 255, null: false
+      t.boolean  "include_in_oec",             null: false
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+    add_index "oec_course_codes", ["dept_code"], name: "index_oec_course_codes_on_dept_code", using: :btree
+    add_index "oec_course_codes", ["dept_name", "catalog_id"], name: "index_oec_course_codes_on_dept_name_and_catalog_id", unique: true, using: :btree
+
+    create_table "webcast_course_site_log", force: :cascade do |t|
+      t.integer  "canvas_course_site_id",    null: false
+      t.datetime "webcast_tool_unhidden_at", null: false
+      t.datetime "created_at",               null: false
+      t.datetime "updated_at",               null: false
+    end
+    add_index "webcast_course_site_log", ["canvas_course_site_id"], name: "webcast_course_site_log_unique_index", unique: true, using: :btree
+  end
+end
