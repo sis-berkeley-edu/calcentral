05a7b3219b1fa6235775520d034669aa29b404d9
Jason Miller <jason@redconfetti.com>
2019-05-18 21:25:54 -0700

    SISRP-45788 - Fixes error with Rails Admin due to equal sign in slow logging callback path

--- config/initializers/slow_request_notifications.rb
+++ config/initializers/slow_request_notifications.rb
@@ -10,7 +10,8 @@ if Settings.logger.slow_request_threshold_in_ms.default.to_i > 0
     unless payload[:db_runtime].nil?
       db_time = payload[:db_runtime]
     end
-    threshold = Settings.logger.slow_request_threshold_in_ms.send(payload[:path]) || Settings.logger.slow_request_threshold_in_ms.default
+    payload_path = payload[:path].split('?').first
+    threshold = Settings.logger.slow_request_threshold_in_ms.send(payload_path) || Settings.logger.slow_request_threshold_in_ms.default
     if duration > threshold.to_i
       Rails.logger.error "SLOW REQUEST #{payload[:path]}; view=#{view_time.to_i}ms db=#{db_time.to_i}ms total=#{duration.to_i}ms"
     end
