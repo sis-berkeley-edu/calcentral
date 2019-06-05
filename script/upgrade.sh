be6b7dd65cc3ba8582e25c6decbb6723d4fb6c59
Jason Miller <jason@redconfetti.com>
2019-06-05 14:53:02 -0700

    SISRP-46771 - Remove database migration calls in CalCentral admin scripts

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -8,19 +8,8 @@
 
 cd $( dirname "${BASH_SOURCE[0]}" )/..
 
-HOST=$(uname -n)
-
-if [[ "${HOST}" = *-01\.ist.berkeley.edu ]]; then
-  IS_NODE_ONE="yes"
-fi
-
 ./script/init.d/calcentral maint
 
 ./script/update-build.sh || { echo "ERROR: update-build failed"; exit 1; }
-
-# run migrate.sh only if we are on node 1
-if [ "X${IS_NODE_ONE}" != "X" ]; then
-  ./script/migrate.sh
-fi
 
 exit 0
