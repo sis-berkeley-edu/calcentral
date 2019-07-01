665dd47da4c38803d0e005a822bdbee17e341183
Brandon Wright <brandon@developish.com>
2019-07-01 11:27:38 -0700

    Attempt to fix omniauth-cas loading issue

--- config/warble.rb
+++ config/warble.rb
@@ -1,4 +1,5 @@
 Warbler::Config.new do |config|
+  config.override_gem_home = false
   config.dirs = %w(app bin config db lib log public script vendor versions tmp)
   config.init_contents << StringIO.new("\nGem.clear_paths\nGem.path\n")
   config.jrubyc_options = "--javac"
