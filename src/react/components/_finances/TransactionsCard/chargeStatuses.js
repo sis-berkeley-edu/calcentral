a8f64194961f57bb19950f645501d3e42e320273
Brandon Wright <brandon@developish.com>
2019-07-31 05:44:52 -0700

    SISRP-45389: Label for due items is "Due Now"

--- src/react/components/_finances/TransactionsCard/chargeStatuses.js
+++ src/react/components/_finances/TransactionsCard/chargeStatuses.js
@@ -1,4 +1,4 @@
 export const CHARGE_OVERDUE = 'Overdue';
-export const CHARGE_DUE = 'Due';
+export const CHARGE_DUE = 'Due Now';
 export const CHARGE_NOT_DUE = 'Not Yet Due';
 export const CHARGE_PAID = 'Paid';
