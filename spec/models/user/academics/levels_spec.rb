fd5228f76f75f2d62948f751418756182875f528
Jason Miller <jason@redconfetti.com>
2019-10-14 17:27:54 -0700

    SISRP-48920 - Refactoring preferred level logic

--- spec/models/user/academics/levels_spec.rb
+++ spec/models/user/academics/levels_spec.rb
@@ -20,4 +20,22 @@ describe User::Academics::Levels do
       expect(result.collect(&:type_code)).to eq ['BOT', 'EOT']
     end
   end
+
+  describe '#preferred_for_career_code' do
+    let(:career_code) { 'GRAD' }
+    let(:result) { subject.preferred_for_career_code(career_code) }
+    context 'when career code is LAW' do
+      let(:career_code) { 'LAW' }
+      it 'returns end of term level' do
+        expect(result.end_of_term?).to eq true
+      end
+    end
+    context 'when career code is not LAW' do
+      let(:career_code) { 'GRAD' }
+      it 'returns beginning of term level' do
+        expect(result.beginning_of_term?).to eq true
+      end
+    end
+  end
+
 end
