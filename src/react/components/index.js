847dadfbe62f56ecff64a2a7a9e1eaad23c9e1d5
Brandon Wright <brandon@developish.com>
2020-01-27 11:58:38 -0700

    SISRP-49978: Tasks Card - Keep FA cards in sync
    Get “incomplete financial aid tasks” count from new tasks card data sources

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -7,7 +7,11 @@ import './_academics/StatusAndHoldsCard/SemesterStatuses';
 import './_academics/StudentProfile/StudentProfileCard';
 import './_dashboard/StudentResources/StudentResourcesContainer';
 import './_dashboard';
+
 import './_finances/BillingSummary/BillingSummaryContainer';
 import './_finances/FinancialResources/FinancialResources';
 import './_finances/TransactionsCard/TransactionsCardContainer';
+import './_finances/AidYearTaskCount';
+import './_finances/FinancialAidTasks';
+
 import './_user_overview/EmploymentAppointments/EmploymentAppointments';
