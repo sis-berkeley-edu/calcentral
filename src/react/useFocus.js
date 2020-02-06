9ddb7ad371b1c742cfb3c538a8f988004720cf03
Brandon Wright <brandon@developish.com>
2020-02-06 15:45:29 -0700

    SISRP-48518: Fix useFocus bug when no current node

--- src/react/useFocus.js
+++ src/react/useFocus.js
@@ -3,7 +3,11 @@ import { useRef, useState, useEffect } from 'react';
 function useFocus() {
   const node = useRef(null);
   const [hasFocus, setHasFocus] = useState(true);
-  const outsideClick = e => setHasFocus(node.current.contains(e.target));
+  const outsideClick = e => {
+    if (node.current) {
+      setHasFocus(node.current.contains(e.target));
+    }
+  };
 
   useEffect(() => {
     document.addEventListener('mousedown', outsideClick);
