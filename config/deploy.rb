77a970724d004107cce77ab65adec24c4a32705d
Susana Parker <susanap@berkeley.edu>
2020-05-19 09:28:42 -0700

    SISRP-52450: Add hot deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,36 +16,29 @@ set :project_root, settings.common.root
 # and 1 elasticsearch server.
 namespace :calcentral_dev do
   desc "Update and restart the calcentral_dev machine"
-  task :update, :roles => :calcentral_dev_host do
-    # Take everything offline first.
-    servers = find_servers_for_task(current_task)
-
-    transaction do
-      servers.each_with_index do |server, index|
-        run "cd #{project_root}; ./script/update-build-tomcat.sh", :hosts => server
-      end
-    end
-  end
-
-  desc "Update and restart the calcentral_dev machine"
   task :colddeploy, :roles => :calcentral_dev_host do
-    # Take everything offline first.
+    # This task is used for BCS cutovers when we will put the system in maintenance mode
+    # without restarting apache
+    # This deployment produces an outage to the user
     servers = find_servers_for_task(current_task)
 
     transaction do
       servers.each_with_index do |server, index|
-        run "cd #{project_root}; ./script/update-build-tomcat-test.sh -o offline", :hosts => server
+        run "cd #{project_root}; ./script/run_deploy_main.sh -w online", :hosts => server
       end
     end
   end
 
   desc "Update CalCentral warfile w/o resting tomcat server"
   task :hotdeploy, :roles => :calcentral_dev_host do
+    # This task will be used for rolling deploy/restarts for multiple nodes
+    # It takes the node out of the load balancer by shutting down apache
+    # This deployment is perceived as a ZERO DOWNTIME deployment - Default
     servers = find_servers_for_task(current_task)
 
     transaction do
       servers.each_with_index do |server, index|
-        run "cd #{project_root}; ./script/update-build-tomcat-test.sh", :hosts => server
+        run "cd #{project_root}; ./script/run_deploy_main.sh", :hosts => server
       end
     end
   end
